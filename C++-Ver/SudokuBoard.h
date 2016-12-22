9aa4e413b7c7b73b9f20969f092e819ce0f1fc95
Darrylx3 <darrylclw@gmail.com>
2016-12-22 18:00:57 +0800

    Initial commit

---
+++ C++-Ver/SudokuBoard.h
@@ -0,0 +1,98 @@
+#ifndef SUDOKU_BOARD_H
+#define SUDOKU_BOARD_H
+#include <iostream>
+using namespace std;
+
+class SudokuBoard{
+	int ** board;
+	int dimension;
+public:
+	SudokuBoard():dimension(9){
+		initializeBoard(); // 9x9
+	}
+
+	void initializeBoard(){
+		//Used for testing the solving algorithm
+		int tempValues[][9] = 	{{3,0,6,5,0,8,4,0,0}
+								,{5,2,0,0,0,0,0,0,0}
+								,{0,8,7,0,0,0,0,3,1}
+								,{0,0,3,0,1,0,0,8,0}
+								,{9,0,0,8,6,3,0,0,5}
+								,{0,5,0,0,9,0,6,0,0}
+								,{1,3,0,0,0,0,2,5,0}
+								,{0,0,0,0,0,0,0,7,4}
+								,{0,0,5,2,0,6,3,0,0}};
+
+		board = new int*[dimension];
+		for(int i = 0; i < dimension; i++){
+			board[i] = new int[dimension];
+
+			//Temporary for tempValues assignment
+			for(int y = 0; y < dimension; y++){
+				board[i][y] = tempValues[i][y];
+			}
+		}
+	}
+
+	void printBoard(){
+		for(int x = 0; x < 9; x++){
+			for(int y = 0; y < 9; y++){
+				cout << board[x][y] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	bool checkColumnIfValid(int col, int num){
+		//Check if _num_ is valid for that column _col_
+		//^Sudoku rules, no duplicate values.
+		//Return false if not valid
+		for(int row = 0; row < dimension; row++){
+			if (board[row][col] == num){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool checkRowIfValid(int row, int num){
+		//Check if _num_ is valid for that row _row_
+		//^Sudoku rules, no duplicate values.
+		//Return false if not valid
+		for(int col = 0; col < dimension; col++){
+			if (board[row][col] == num){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool checkBoxIfValid(int row, int col, int num){
+		//Check if _num_ is valid for that box
+		//Each value belongs to a row, column and in a box.
+		//^Sudoku rules, no duplicate values.
+		//Return false if not valid
+
+		//boxSize needs to be dynamic when increasing sudoku dimension
+		int boxSize = 3; 
+
+		for (int x = (row/boxSize)*boxSize; x < (row/boxSize)*boxSize+boxSize; ++x){
+			for (int y = (col/boxSize)*boxSize; y < (col/boxSize)*boxSize+boxSize; ++y){
+				if (board[x][y] == num){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	//One call method to call the other 3 methods
+	//Checks the sudoku rules if the number is valid on that board location
+	bool checkLocationIfValid(int row, int col, int num){
+		//Returns false if NOT valid
+		return checkBoxIfValid(row,col,num) && checkColumnIfValid(col,num) && checkRowIfValid(row,num);
+	}
+};
+
+
+#endif
