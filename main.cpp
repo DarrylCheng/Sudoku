0d9df3dbcb575685f2a9bfd76b7866177b2c0203
Darrylx3 <darrylclw@gmail.com>
2017-01-08 21:58:48 +0800

    Added chrono to calculate elapse time

--- main.cpp
+++ main.cpp
@@ -4,25 +4,51 @@
 #include "GenerateSudoku.h"
 #include "DifficultyLevels/Level.h"
 #include <windows.h>
+#include <chrono>
+#include <fstream>
 using namespace std;
 //Compile in C++11 --std=c++11
 //If error, add --std=gnu++0x
 
 int main(){
-	// SudokuBoard sb;
-	// sb.printBoard();
-	// cout << endl;
-	// Backtracking solve(sb);
-	// cout << boolalpha << endl << solver.solve() << endl;
-	// solver.printSolvedBoard();
-
 	GenerateSudoku sb;
-	SudokuBoard yo;
-	while(true){
-		//Test the generate function for random complete sudoku board.
-		yo = sb.generate(Level::MEDIUM);
-		yo.printBoard();
-		cout << endl;
-		Sleep(700);
+	SudokuBoard easy, medium, difficult, evil;
+	ofstream file;
+	file.open("results.csv");
+	file << "Easy,Medium,Difficult,Evil\n";
+	for(int i=0; i< 10000; i++){
+		easy = sb.generate(Level::EASY);
+		medium = sb.generate(Level::MEDIUM);
+		difficult = sb.generate(Level::DIFFICULT);
+		evil = sb.generate(Level::EVIL);
+
+		Backtracking solver;
+		auto start = chrono::system_clock::now();
+		solver.solve(easy);
+		auto end = chrono::system_clock::now();
+		chrono::duration<double> duration = end - start;
+		// cout << "\nTime elapsed (Easy): " << duration.count();
+		file << duration.count() << ",";
+
+		start = chrono::system_clock::now();
+		solver.solve(medium);
+		end = chrono::system_clock::now();
+		duration = end - start;
+		// cout << "\nTime elapsed (medium): " << duration.count();
+		file << duration.count() << ",";
+
+		start = chrono::system_clock::now();
+		solver.solve(difficult);
+		end = chrono::system_clock::now();
+		duration = end - start;
+		// cout << "\nTime elapsed (difficult): " << duration.count();
+		file << duration.count() << ",";
+
+		start = chrono::system_clock::now();
+		solver.solve(evil);
+		end = chrono::system_clock::now();
+		duration = end - start;
+		// cout << "\nTime elapsed (evil): " << duration.count();
+		file << duration.count() << "\n";
 	}
 }
