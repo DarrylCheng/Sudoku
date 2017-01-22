f5bd2f4521849d5b0b3a5aa5002104640772a858
Darrylx3 <darrylclw@gmail.com>
2017-01-22 17:10:47 +0800

    After every successful trial, will add results to Result.csv

--- main.cpp
+++ main.cpp
@@ -1,54 +1,59 @@
 #include <iostream>
+#include <chrono>
+#include <fstream>
+#include "DifficultyLevels/Level.h"
 #include "Configuration.h"
 #include "SudokuPuzzle.h"
 #include "SudokuBoard.h"
 #include "Backtracking.h"
-#include "DifficultyLevels/Level.h"
-// #include <windows.h>
-#include <chrono>
 #include "ConstraintP.h"
-// #include <fstream>
 using namespace std;
 //Compile in C++11 --std=c++11
 //If error, add --std=gnu++0x
 
 int dimension, givens, boxSizeX, boxSizeY;
-// int dimension = 4,givens = 4, boxSizeX = 2, boxSizeY = 2, holesDig = 1; // 4
-// int dimension = 9,givens = 11, boxSizeX = 3, boxSizeY = 3, holesDig = 1; //9
-// int dimension = 12,givens = 15, boxSizeX = 4, boxSizeY = 3, holesDig = 1; //12
-// int dimension = 18,givens = 20, boxSizeX = 6, boxSizeY = 3, holesDig = 1; //18
+Level level;
 
 int main(int argc, char * argv[]){
-	if(argc==1){
-		dimension = 9, givens = 11, boxSizeX = 3, boxSizeY = 3;
-	} else if((string)argv[1] == "4"){
-		dimension = 4, givens = 4, boxSizeX = 2, boxSizeY = 2;
-	} else if((string)argv[1] == "12"){
-		dimension = 12, givens = 15, boxSizeX = 4, boxSizeY = 3;
-	} else if((string)argv[1] == "18"){
-		dimension = 18, givens = 20, boxSizeX = 6, boxSizeY = 3;
+	if(argc>1){
+		if((string)argv[1] == "4"){
+			dimension = 4, givens = 4, boxSizeX = 2, boxSizeY = 2; //4x4
+		} else if((string)argv[1] == "9"){
+			dimension = 9,givens = 11, boxSizeX = 3, boxSizeY = 3; //9x9
+		} else if((string)argv[1] == "12"){
+			dimension = 12, givens = 15, boxSizeX = 4, boxSizeY = 3; //12x12
+		} else if((string)argv[1] == "18"){
+			dimension = 18, givens = 20, boxSizeX = 6, boxSizeY = 3; //18x18
+		} 
+		if(argc > 2){
+			string lvl = (string)argv[2];
+			level = (lvl=="4"?Level::EVIL:lvl=="2"?Level::MEDIUM:lvl=="3"?Level::DIFFICULT:Level::EASY);
+		} else {
+			level = Level::EASY; //Default level
+		}
 	} else {
-		dimension = 9,givens = 11, boxSizeX = 3, boxSizeY = 3;
+		dimension = 9,givens = 11, boxSizeX = 3, boxSizeY = 3, level = Level::EASY; //9x9 default
 	}
 
 	//Generate sudoku
 	auto start = chrono::system_clock::now();
-	SudokuPuzzle pz1(Level::EASY,dimension);
+	SudokuPuzzle pz1(level,dimension);
 	SudokuPuzzle pz2(pz1);
+	SudokuPuzzle backup(pz1);
 	auto end = chrono::system_clock::now();
-	chrono::duration<double> duration = end - start;
+	chrono::duration<double> durationGenerate = end - start;
 	pz2.print();
-	cout << "Time elapsed (Generating): " << duration.count() << "\n\n";
+	cout << "Time elapsed (Generating): " << durationGenerate.count() << "\n\n";
 
 	//Try solving with backtracking algorithm
 	Backtracking bt;
 	start = chrono::system_clock::now();
-	bool solvable = bt.solve(pz1.getBoard()); //bt solving requires SudokuBoard as parameter instead of SudokuPuzzle
+	bool solvableBT = bt.solve(pz1.getBoard()); //bt solving requires SudokuBoard as parameter instead of SudokuPuzzle
 	end = chrono::system_clock::now();
-	duration = end - start;
-	if(solvable){
+	chrono::duration<double> durationBT = end - start;
+	if(solvableBT){
 		pz1.print();
-		cout << "Time elapsed (BTSolving): " << duration.count() << endl;
+		cout << "Time elapsed (BTSolving): " << durationBT.count() << endl;
 	} else {
 		cout << "Sudoku board is not solvable.\n";
 	}
@@ -56,61 +61,28 @@ int main(int argc, char * argv[]){
 	//Test constraint propogation
 	ConstraintP cp;
 	start = chrono::system_clock::now();
-	cp.populateCells(pz2.getBoard());
-	bool lol = cp.assignValueWithOneChoice(pz2.getBoard());
-	if(!lol){
-		// cout << "Continue with backtracking\n";
-		Backtracking bt;
-		bt.solve(pz2.getBoard());
-	}
+	bool solvableCP = cp.solve(pz2.getBoard());
 	end = chrono::system_clock::now();
-	pz2.print();
-	duration = end - start;
-	cout << "Time elapsed (CPSolving): " << duration.count() << endl;
-
-
-
-
-	// SudokuBoard easy, medium, difficult, evil;
-	// SudokuBoard easy(18);
-	// easy = sb.generate(Level::EASY,18);
-	// ofstream file;
-	// file.open("results.csv");
-	// file << "Easy,Medium,Difficult,Evil\n";
-	for(int i=0; i< 1; i++){
-		// easy = sb.generate(Level::EASY,18);
-		// medium = sb.generate(Level::MEDIUM);
-		// difficult = sb.generate(Level::DIFFICULT);
-		// evil = sb.generate(Level::EVIL);
-
-		// Backtracking solver;
-		// auto start = chrono::system_clock::now();
-		// solver.solve(easy);
-		// easy.printBoard();
-		// auto end = chrono::system_clock::now();
-		// chrono::duration<double> duration = end - start;
-		// // cout << "\nTime elapsed (Easy): " << duration.count();
-		// file << duration.count() << ",";
-
-		// start = chrono::system_clock::now();
-		// solver.solve(medium);
-		// end = chrono::system_clock::now();
-		// duration = end - start;
-		// // cout << "\nTime elapsed (medium): " << duration.count();
-		// file << duration.count() << ",";
-
-		// start = chrono::system_clock::now();
-		// solver.solve(difficult);
-		// end = chrono::system_clock::now();
-		// duration = end - start;
-		// // cout << "\nTime elapsed (difficult): " << duration.count();
-		// file << duration.count() << ",";
-
-		// start = chrono::system_clock::now();
-		// solver.solve(evil);
-		// end = chrono::system_clock::now();
-		// duration = end - start;
-		// // cout << "\nTime elapsed (evil): " << duration.count();
-		// file << duration.count() << "\n";
+	chrono::duration<double> durationCP = end - start;
+	if(solvableCP){
+		pz2.print();
+		cout << "Time elapsed (CPSolving): " << durationCP.count() << endl;
+	} else {
+		cout << "Sudoku board is not solvable.\n";
 	}
+
+	ofstream file;
+	file.open("Results.csv", ios::app);
+	file << backup << "," << pz1 << "," << dimension << "x" << dimension << "," << boxSizeX << "," << boxSizeY;
+	if(level == Level::EASY)
+		file << "," << "EASY";
+	else if (level == Level::MEDIUM)
+		file << "," << "MEDIUM";
+	else if (level == Level::DIFFICULT)
+		file << "," << "DIFFICULT";
+	else 
+		file << "," << "EVIL";
+	file << "," << durationGenerate.count() << "," << durationBT.count() << "," << solvableBT << "," << durationCP.count() << "," << solvableCP;
+	file << endl;
+	file.close();
 }
