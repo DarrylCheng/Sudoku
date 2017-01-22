ceaffe37c3a4c0056503e2b444972a04ddb71bee
Darrylx3 <darrylclw@gmail.com>
2017-01-22 17:08:54 +0800

    Added levels to configuration

--- Configuration.h
+++ Configuration.h
@@ -1,8 +1,10 @@
 #ifndef Configuration_H
 #define Configuration_H
+#include "DifficultyLevels/Level.h"
 
 extern int givens;
 extern int boxSizeX; //X*Y
 extern int boxSizeY;
+extern Level level;
 
 #endif
